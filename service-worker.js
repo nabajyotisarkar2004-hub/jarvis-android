1dcbfc63827195d9a15922c2ca18bd9b17e13578
nabajyotisarkar2004-hub <nabajyotisarkar2004@gmail.com>
2025-12-19 08:45:14 +0530

    Update cache files in service worker

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,16 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("jarvis-cache").then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js"
+        "index.html",
+        "style.css",
+        "script.js"
       ]);
     })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
